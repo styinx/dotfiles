ed4b3ca356c0aa3d88a9a7a244bfae3edc121d24
Chris <zornchrisjo@gmail.com>
2026-07-05 19:54:17 +0200

    update queries

---
+++ .config/nvim/queries/tri/highlights.scm
@@ -0,0 +1,114 @@
+; -----------------------------------------------------------------------------
+; Fallback
+; -----------------------------------------------------------------------------
+
+(identifier) @variable
+
+; -----------------------------------------------------------------------------
+; Literals
+; -----------------------------------------------------------------------------
+
+(boolean) @boolean
+(nil) @constant.builtin
+(nop) @constant.builtin
+
+(number) @number
+(int) @number
+(float) @number.float
+(bin) @number
+(hex) @number
+
+(string) @string
+
+; -----------------------------------------------------------------------------
+; Types
+; -----------------------------------------------------------------------------
+
+(type) @type
+(builtin_type) @type.builtin
+
+; -----------------------------------------------------------------------------
+; Operators
+; -----------------------------------------------------------------------------
+
+"="  @operator
+"->" @operator
+
+":"  @punctuation.delimiter
+","  @punctuation.delimiter
+".." @punctuation.delimiter
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+
+; -----------------------------------------------------------------------------
+; Keywords
+; -----------------------------------------------------------------------------
+
+[
+  "alias"
+  "beg"
+  "elif"
+  "else"
+  "end"
+  "for"
+  "fun"
+  "if"
+  "in"
+  "mod"
+  "rec"
+  "ret"
+  "type"
+] @keyword
+
+; -----------------------------------------------------------------------------
+; Declarations
+; -----------------------------------------------------------------------------
+
+(alias_declaration
+  name: (identifier) @type)
+
+(function_declaration
+  name: (identifier) @function)
+
+(module_declaration
+  name: (identifier) @module)
+
+(record_declaration
+  name: (identifier) @type)
+
+(type_declaration
+  name: (identifier) @type)
+
+(variable_declaration
+  name: (identifier) @variable.parameter)
+
+; -----------------------------------------------------------------------------
+; Parameters
+; -----------------------------------------------------------------------------
+
+(parameter_declaration_list
+  (variable_declaration
+    name: (identifier) @variable.parameter))
+
+; -----------------------------------------------------------------------------
+; Expressions
+; -----------------------------------------------------------------------------
+
+(call_expression
+  name: (identifier) @function.call)
+
+(operation_expression
+  operator: (operator) @operator)
+
+; -----------------------------------------------------------------------------
+; Statements
+; -----------------------------------------------------------------------------
+
+(for_statement
+  counter: (identifier) @variable)
+
+(return_statement
+  "ret" @keyword.return)
